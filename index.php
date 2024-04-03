ece33e3e4061b8eb6e49a28791657e44d3cce975
salvatore_barbaro <salvatore.ilbarbaro97@gmail.com>
2024-04-04 01:10:25 +0200

    Esercizio completato esclusi i bonus

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
                     <h5 class="card-title fs-2 text-uppercase text-success  "><?php echo ($Element->Name) ?></h5>
                     <p class="card-text"><span class="fs-5">Descrizione: </span><?php echo ($Element->Description) ?></p>
                     <p class=""><span class="fs-5">Prezzo: </span><?php echo ($Element->Price) ?>$</p>
-                    <div class=" position-absolute top-0 end-0  bg-white opacity-75 p-1 border rounded-3 fs-5 "><?php echo ($Element->Icona )?></div>
+                    <div class=" position-absolute top-0 end-0  bg-white opacity-75 p-1 border rounded-3 fs-5"><?php echo ($Element->Icona )?></div>
                     <a href="#" class="btn btn-primary text-uppercase ">Compra</a>
                 </div>
         </div>
